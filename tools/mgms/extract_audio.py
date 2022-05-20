5118d2c646c2d7854cb47729acf1b8774d42b02b
yingfeng-iu <yingfeng@iu.edu>
2022-05-19 22:34:04 -0400

    AMP-1916: more type and improve help

--- tools/mgms/extract_audio.py
+++ tools/mgms/extract_audio.py
@@ -14,10 +14,10 @@ def main():
     parser = argparse.ArgumentParser(description="Extract the audio stream from a file as-is")
     parser.add_argument('--debug', default=False, action='store_true', help="Turn on debugging")    
     parser.add_argument('avfile', help="Input A/V file")
-    parser.add_argument('rate', type=int, help="Audio rate")
-    parser.add_argument('channels', type=int, help="Audio channels")
-    parser.add_argument('sample_format', help="Sample format (i.e. pcm_s16le)")
-    parser.add_argument('audiostream', help="Output filename")
+    parser.add_argument('rate', type=int, default=8000, help="Sample rate")
+    parser.add_argument('channels', type=int, default=1, help="Number of channels")
+    parser.add_argument('sample_format', default="pcm_s16le", help="Sample format (i.e. pcm_s16le)")
+    parser.add_argument('audio_extracted', help="Audio Extracted")
     args = parser.parse_args()    
     logging.info(f"Starting with args {args}") 
     # use ffmpeg to extract the audio stream and put it into the file
@@ -29,7 +29,7 @@ def main():
                         '-ar', str(args.rate),
                         '-ac', str(args.channels), 
                         '-f', 'wav', 
-                        args.audiostream])
+                        args.audio_extracted])
     if p.returncode:
         logging.error(f"FFMPEG returned non-zero return code: {p.returncode}")
         exit(1)
