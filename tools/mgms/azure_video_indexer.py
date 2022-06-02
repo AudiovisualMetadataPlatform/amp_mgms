4e17966821869b407ac91479f2a88a07fd7db4fe
yingfeng-iu <yingfeng@iu.edu>
2022-06-01 22:30:41 -0400

    AMP-1788: improve log

--- tools/mgms/azure_video_indexer.py
+++ tools/mgms/azure_video_indexer.py
@@ -191,9 +191,9 @@ def upload_to_s3(input_video, bucket):
     jobname = str(uuid.uuid1())
     try:
         response = s3_client.upload_file(input_video, bucket, jobname, ExtraArgs={'ACL': 'public-read'})
-        logging.info(f"Uploaded file {input_video} to s3 bucket {bucket}")
+        logging.info(f"Uploaded file {input_video} to S3 bucket {bucket}")
     except Exception as e:
-        logging.error(f"Failed to upload file {input_video} to s3 bucket {bucket}:\n{e}")
+        logging.error(f"Failed to upload file {input_video} to S3 bucket {bucket}:\n{e}")
         traceback.print_exc()
         return None
     return jobname
@@ -203,9 +203,9 @@ def delete_from_s3(s3_path, bucket):
     try:
         obj = s3_client.Object(bucket, s3_path)
         obj.delete()
-        logging.info(f"Deleted file {s3_path} from s3 bucket {bucket}")
+        logging.info(f"Deleted file {s3_path} from S3 bucket {bucket}")
     except Exception as e:
-        logging.warning(f"Failed to delete file {s3_path} from s3 bucket {bucket}:\n{e}")
+        logging.warning(f"Failed to delete file {s3_path} from S3 bucket {bucket}:\n{e}")
 
 
 if __name__ == "__main__":
