6a7d79139ab260b9bacced99801158c97baf51ce
Brian Wheeler <bdwheele@iu.edu>
2022-09-21 13:06:05 -0400

    cleanup azure mgms

--- tools/azure/azure_video_indexer.py
+++ tools/azure/azure_video_indexer.py
@@ -12,47 +12,41 @@ import http.client as http_client
 
 import amp.logging
 from amp.config import load_amp_config, get_config_value, get_cloud_credentials
+from amp.fileutils import write_json_file
 
 def main():
     apiUrl = "https://api.videoindexer.ai"
-
-    #(root_dir, input_video, include_ocr, region_name, azure_video_index, azure_artifact_ocr) = sys.argv[1:7]
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", default=False, action="store_true", help="Turn on debugging")
     parser.add_argument("input_video", help="Input video file")
-    parser.add_argument("include_ocr", type=strtobool, default=True, help="Include OCR")
+    parser.add_argument("--include_ocr", type=strtobool, default=True, help="Include OCR")
     parser.add_argument("azure_video_index", help="Azure Video Index JSON")
     parser.add_argument("azure_artifact_ocr", help="Azure Artifact OCR JSON")
     args = parser.parse_args()
     amp.logging.setup_logging("azure_video_indexer", args.debug)
     logging.info(f"Starting with args {args}")
-    (input_video, include_ocr, azure_video_index, azure_artifact_ocr) = (args.input_video, args.include_ocr, args.azure_video_index, args.azure_artifact_ocr)
     
     config = load_amp_config()
-
     azure = get_cloud_credentials(config, 'azure')
     account_id = azure['account_id']
     api_key = azure['api_key']
     region_name = azure['region_name']
-
-
     s3_bucket = get_config_value(config, ['mgms', 'azure_video_indexer', 's3_bucket'])
     
-
     # Turn on HTTP debugging here
     http_client.HTTPConnection.debuglevel = 1
 
     aws_creds = get_cloud_credentials(config, 'aws')
 
     # upload video to S3
-    s3_path = upload_to_s3(aws_creds, input_video, s3_bucket)
+    s3_path = upload_to_s3(aws_creds, args.input_video, s3_bucket)
     
     # Get an authorization token for subsequent requests
     auth_token = get_auth_token(apiUrl, region_name, account_id, api_key)
     
     # Upload the video and get the ID to reference for indexing status and results
     video_url = "https://" + s3_bucket + ".s3.us-east-2.amazonaws.com/" + s3_path
-    videoId = index_video(apiUrl, region_name, account_id, auth_token, input_video, video_url)
+    videoId = index_video(apiUrl, region_name, account_id, auth_token, args.input_video, video_url)
 
     # Check on the indexing status
     while True:
@@ -73,21 +67,21 @@ def main():
     # Get the simple video index json
     auth_token = get_auth_token(apiUrl, region_name, account_id, api_key)
     index_json = get_video_index_json(apiUrl, region_name, account_id, videoId, auth_token, api_key)
-    write_json_file(index_json, azure_video_index)
+    write_json_file(index_json, args.azure_video_index)
 
     # Get the advanced OCR json via the artifact URL if requested
-    if include_ocr:
+    if args.include_ocr:
         try:
             artifacts_url = get_artifacts_url(apiUrl, region_name, account_id, videoId, auth_token, 'ocr')
-            download_artifacts(artifacts_url, azure_artifact_ocr)
-            logging.info(f"Downloaded OCR artifact to {azure_artifact_ocr}")
+            download_artifacts(artifacts_url, args.azure_artifact_ocr)
+            logging.info(f"Downloaded OCR artifact to {args.azure_artifact_ocr}")
         # When the video doesn't contain OCR, no OCR artifact will be generated, and the above download will result in exception.
         # Since we may not know before hand if a video contains OCR or not, this should be a normal case and not cause job failure;
         # instead, we can generate an empty OCR output, and the next job should handle such case and generate empty AMP VOCR.
         # However, the exception could also be caused by system errors, in which case job should fail.
         # TODO should we fail the job or generate an empty OCR file ?
         except:
-            logging.exception(f"Failed to download OCR artifact to {azure_artifact_ocr}!")    
+            logging.exception(f"Failed to download OCR artifact to {args.azure_artifact_ocr}!")    
     
     delete_from_s3(aws_creds, s3_path, s3_bucket)
     logging.info("Finished.")
--- tools/azure/azure_video_ocr.py
+++ tools/azure/azure_video_ocr.py
@@ -3,7 +3,6 @@ import logging
 from distutils.util import strtobool
 import argparse
 import logging
-
 import amp.logging
 from amp.timeutils import frameToSecond
 from amp.fileutils import read_json_file, write_json_file, valid_file
@@ -16,33 +15,32 @@ def main():
 	parser.add_argument("input_video", help="Video input file")
 	parser.add_argument("azure_video_index", help="Azure Video Index input file")
 	parser.add_argument("azure_artifact_ocr", help="Azure Artifact OCRinput file")
-	parser.add_argument("dedupe", type=strtobool, default=True, help="Whether to dedupe consecutive frames with same texts")
-	parser.add_argument("dup_gap", type=int, default=5, help="Gap in seconds within which adjacent VOCR frames with same text are considered duplicates")	
+	parser.add_argument("--dedupe", type=strtobool, default=True, help="Whether to dedupe consecutive frames with same texts")
+	parser.add_argument("--dup_gap", type=int, default=5, help="Gap in seconds within which adjacent VOCR frames with same text are considered duplicates")	
 	parser.add_argument("amp_vocr", help="Original AMP Video OCR output file")
 	parser.add_argument("amp_vocr_dedupe", help="Deduped AMP Video OCR output file")
 	args = parser.parse_args()
 	amp.logging.setup_logging("azure_video_ocr", args.debug)
-	logging.info(f"Starting with args {args}")
-	(input_video, azure_video_index, azure_artifact_ocr, dedupe, dup_gap, amp_vocr, amp_vocr_dedupe) = (args.input_video, args.azure_video_index, args.azure_artifact_ocr, args.dedupe, args.dup_gap, args.amp_vocr, args.amp_vocr_dedupe)
+	logging.info(f"Starting with args {args}")	
 	
 	# get Azure video indexer json
-	azure_index_json = read_json_file(azure_video_index)
+	azure_index_json = read_json_file(args.azure_video_index)
 
 	# get Azure artifact OCR json
 	# in case Azure Indexer didn't produce OCR artifact, pass in empty json
-	azure_ocr_json = read_json_file(azure_artifact_ocr) if valid_file(azure_artifact_ocr) else None
+	azure_ocr_json = read_json_file(args.azure_artifact_ocr) if valid_file(args.azure_artifact_ocr) else None
 
 	# create AMP Video OCR object
-	vocr = create_amp_vocr(input_video, azure_index_json, azure_ocr_json)
+	vocr = create_amp_vocr(args.input_video, azure_index_json, azure_ocr_json)
 	
 	# write AMP Video OCR JSON file
-	write_json_file(vocr, amp_vocr)
+	write_json_file(vocr, args.amp_vocr)
 	logging.info(f"Successfully generated AMP VOCR with {len(vocr.frames)} original frames.")
 	
 	# if dedupe, create the deduped AMP VOCR
-	if dedupe:
-		vocr_dedupe = vocr.dedupe(dup_gap)
-		write_json_file(vocr_dedupe, amp_vocr_dedupe)
+	if args.dedupe:
+		vocr_dedupe = vocr.dedupe(args.dup_gap)
+		write_json_file(vocr_dedupe, args.amp_vocr_dedupe)
 		logging.info(f"Successfully deduped AMP VOCR to {len(vocr_dedupe.frames)} frames.")			
 	
 	
@@ -78,32 +76,11 @@ def create_amp_vocr(input_video, azure_index_json, azure_ocr_json):
 
 # Create a list of AMP VOCR texts from the texts list in the given Azure indexer ocr insight json.
 def createVocrTexts(ocr_json):
+	# BDW: this function doesn't really do anything.
 	texts = []
 	for ocr in ocr_json:
 		for instance in ocr["instances"]:
 			text = None
-# 					# Get where this term starts and end in terms of frame number
-# 					frameIndexStart = getFrameIndex(i["start"], frameRate)
-# 					frameIndexEnd = getFrameIndex(i["end"], frameRate)
-# 					# Create a temp obj to store the results
-# 					newOcr = {
-# 						"text" : ocr["text"],
-# 						"language" : ocr["language"],
-# 						"confidence" : ocr["confidence"],
-# 						"left" : ocr["left"],
-# 						"top" : ocr["top"],
-# 						"width" : ocr["width"],
-# 						"height" : ocr["height"]
-# 					}
-# 					# From the first frame to the last, add the objects info
-# 					for frameIndex in range(frameIndexStart, frameIndexEnd + 1):
-# 						# If it already exists, append it.  Otherwise create new list
-# 						if frameIndex in frame_dict.keys():
-# 							thisFrame = frame_dict[frameIndex]
-# 							thisFrame.append(newOcr)
-# 							frame_dict[frameIndex] = thisFrame
-# 						else:
-# 							frame_dict[frameIndex] = [newOcr]
 	return texts
 
 
@@ -126,16 +103,16 @@ def createVocrFrames(results_json, fps):
 		for page in result["Ocr"]["pages"]:
 			# we use words instead of lines, because the confidence is only available for each word
 			for word in page["words"]:
-			 	text = word["content"]
-			 	confidence = word["confidence"]
-			 	score = VideoOcrObjectScore("confidence", confidence)
-			 	box = word["boundingBox"]	# box[0:7] = (xl, yt, xr, yt, xr, yb, xl, yb)
-			 	vertices = VideoOcrObjectVertices(box[0], box[1], box[4], box[5])
-			 	# language is not provided for words, but available in the ocr insight texts
-			 	# we could get the language by matching content with ocr text, but its computational expensive
-			 	# it's better just to add language in the texts list instead of frames list
-			 	object = VideoOcrObject(text, "", score, vertices)
-			 	objects.append(object)
+				text = word["content"]
+				confidence = word["confidence"]
+				score = VideoOcrObjectScore("confidence", confidence)
+				box = word["boundingBox"]	# box[0:7] = (xl, yt, xr, yt, xr, yb, xl, yb)
+				vertices = VideoOcrObjectVertices(box[0], box[1], box[4], box[5])
+				# language is not provided for words, but available in the ocr insight texts
+				# we could get the language by matching content with ocr text, but its computational expensive
+				# it's better just to add language in the texts list instead of frames list
+				object = VideoOcrObject(text, "", score, vertices)
+				objects.append(object)
 				
 		frame = VideoOcrFrame(start, content, objects)
 		frames.append(frame)
