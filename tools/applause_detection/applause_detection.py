c06193364ca762343cf5ccbd7080f240d4477ae4
Brian Wheeler <bdwheele@iu.edu>
2022-09-21 08:43:52 -0400

    clean up applause_detectin

--- tools/applause_detection/applause_detection.py
+++ tools/applause_detection/applause_detection.py
@@ -9,43 +9,37 @@ import tempfile
 import argparse
 import logging
 
+# NOTE:  since this is using the non-container python3, the PYTHONPATH may
+# need to be constructed to find the amp standard library
 import amp.logging
 
 
 def main():
-    #(root_dir, input_audio, min_segment_duration, amp_segments) = sys.argv[1:5]
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", default=False, action="store_true", help="Turn on debugging")
-    parser.add_argument("input_audio")
-    parser.add_argument("min_segment_duration", type=int, default=1000)
+    parser.add_argument("--min_segment_duration", type=int, default=1000)
+    parser.add_argument("input_audio")    
     parser.add_argument("amp_segments")
     args = parser.parse_args()
     amp.logging.setup_logging("applause_detection", args.debug)
 
-    logging.info(f"Starting with args={args}")
-    (input_audio, min_segment_duration, amp_segments) = (args.input_audio, args.min_segment_duration, args.amp_segments)    
-    
-    logging.debug("Current directory: " + os.getcwd())
-    logging.debug("Input audio: " + input_audio)
-    
+    logging.info(f"Starting with args={args}")    
+
     # use a tmp directory accessible to the singularity for input/output
-    with tempfile.TemporaryDirectory(dir = "/tmp") as tmpdir:
+    with tempfile.TemporaryDirectory() as tmpdir:
         # copy the input audio file to the tmp directory
-        filename = os.path.basename(input_audio)
-        shutil.copy(input_audio, f"{tmpdir}/{filename}")
-        logging.info("Temporary directory " + tmpdir + " after input file copied: " + str(os.listdir(tmpdir)))
+        filename = os.path.basename(args.input_audio)
+        shutil.copy(args.input_audio, f"{tmpdir}/{filename}")        
         
-        # The applause_detection singularity file is assumed to be @ {mgm_sif}/applause_detection.sif
-        # the new build puts the sif next to the script.
+        # The applause_detection singularity file is assumed to be next to the 
+        # script.
         sif = sys.path[0] + "/applause_detection.sif"
 
         # run singularity
-        subprocess.run([sif, tmpdir, str(min_segment_duration)], check=True)
+        subprocess.run([sif, tmpdir, str(args.min_segment_duration)], check=True)
 
-        # copy the corresponding temporary output file to the output AMP segments JSON
-        logging.info("Temporary directory " + tmpdir + " after output file generated: " + str(os.listdir(tmpdir)))   
-        shutil.copy(f"{tmpdir}/{filename}.json", amp_segments)
-        logging.debug("Output AMP Segment: " + amp_segments)
+        # copy the corresponding temporary output file to the output AMP segments JSON        
+        shutil.copy(f"{tmpdir}/{filename}.json", args.amp_segments)        
     logging.info("Finished")
     exit(0)
 
