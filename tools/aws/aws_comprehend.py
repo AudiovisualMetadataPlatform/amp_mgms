ec4e4efadcb37702f60f49921a5cf786bcf8544d
Brian Wheeler <bdwheele@iu.edu>
2023-02-21 09:37:08 -0500

    work in progress on aws_comprehend.

--- tools/aws/aws_comprehend.py
+++ tools/aws/aws_comprehend.py
@@ -26,23 +26,19 @@ def main():
     parser.add_argument("aws_entities", help="Output aws entities file")
     parser.add_argument("amp_entities", help="Output amp entities file")
     parser.add_argument("--ignore_types", default="QUANTITY, DATE", help="Types of things to ignore")
-#     parser.add_argument("--bucket", default='', help="S3 bucket to use")
-#     parser.add_argument("--dataAccessRoleArn", default='', help="AWS ARN to use")
     parser.add_argument("--force", default=False, action="store_true", help="delete any existing jobs with this name and force a new job")
+    parser.add_argument("--lwlw", default=False, action="store_true", help="Run as an LWLW job")
 
     args = parser.parse_args()
     logging.info(f"Starting with args {args}")
     (amp_transcript, aws_entities, amp_entities, ignore_types) = (args.amp_transcript, args.aws_entities, args.amp_entities, args.ignore_types)
-#     (amp_transcript, aws_entities, amp_entities, ignore_types, bucket, dataAccessRoleArn) = (args.amp_transcript, args.aws_entities, args.amp_entities, args.ignore_types, args.bucket, args.dataAccessRoleArn)
 
     # fixup the default arguments...
     config = amp.utils.get_config()
-#     if dataAccessRoleArn == '':
     role_arn = config.get('aws_comprehend', 'role_arn', fallback=None)
     if role_arn is None:
         logging.error('aws_comprehend/role_arn is not specified in the config file')
         exit(1)
-#     if s3_bucket == '':
     s3_bucket = config.get('aws_comprehend', 's3_bucket', fallback=None)
     if s3_bucket is None:
         logging.error("aws_comprehend/s3_bucket is not specified in the config file")
@@ -58,121 +54,84 @@ def main():
 
     # if we reach here, further processing is needed, continue with preparation for AWS Comprehend job 
     # generate a job name, using the output filename as the basis.    
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    # hostname + timestamp should ensure unique job name
-    jobname = 'AWSC-' + socket.gethostname() + "-" + timestamp 
-#     jobname = "AWST-" + platform.node().split('.')[0] + args.output_file.replace('/', '-')
-#     logging.debug(f"Generated job name {jobname}")
+    job_name = "AWSC-" + platform.node().split('.')[0] + args.output_file.replace('/', '-')
+    logging.debug(f"Generated job name {job_name}")
 
-    if s3_directory != '':
-        object_name = f"{s3_directory}/{jobname}"
-    else:
-        object_name = jobname        
-    s3uri = 's3://' + s3_bucket + '/'
+    if args.force:
+        cleanup_job(job_name)
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # write AMP Transcript text into the input file in a temp directory and upload it to S3
-        #tmpdir = tempfile.mkdtemp(dir="/tmp")
-        upload_input_to_s3(amp_transcript_obj, tmpdir, s3_bucket, jobname)
-
-        # Make call to AWS Comprehend
-        outputuri = run_comprehend_job(jobname, s3uri, role_arn)
-
-        # download AWS Comprehend output from s3 to the tmp directory, uncompress and copy it to output aws_entities output file
-        download_output_from_s3(outputuri, s3uri, s3_bucket, tmpdir, aws_entities)
-
-        # AWS Comprehend output should contain entities
-        aws_entities_json = amp.utils.read_json_file(aws_entities)
-        if not 'Entities' in aws_entities_json.keys():
-            logging.error(f"Error: AWS Comprehend output does not contain entities list")
-            exit(1)
-        aws_entities_list = aws_entities_json["Entities"]
-
-        # populate AMP Entities list based on input AMP transcript words list and output AWS Entities list  
-        amp.ner_helper.populate_amp_entities(amp_transcript_obj, aws_entities_list, amp_entities_obj, ignore_types_list)
-
-        # write the output AMP entities to JSON file
-        amp.utils.write_json_file(amp_entities_obj, amp_entities)
-    logging.info("Finished.")
-    
-# Upload the transcript file created from amp_transcript_obj in tmpdir to the S3 bucket for the given job. 
-def upload_input_to_s3(amp_transcript_obj, tmpdir, bucket, jobname):
-    # write the transcript text into a tmp input file
-    try:
-        # use jobname as input filename
-        input = tmpdir + "/" + jobname
-        with open(input, 'w') as infile:
-            infile.write(amp_transcript_obj.results.transcript)
-            logging.info(f"Successfully created input file {input} containing transcript for AWS Comprehend job.")
-    except Exception as e:
-        logging.exception(f"Exception while creating input file {input} containing transcript for AWS Comprehend job.")
-        raise
-    
-    # upload the tmp file to s3
-    try:
-        s3_client = boto3.client('s3', **amp.utils.get_aws_credentials())
-        response = s3_client.upload_file(input, bucket, jobname)
-        logging.info(f"Successfully uploaded input file {input} to S3 bucket {bucket} for AWS Comprehend job.")
-    except Exception as e:
-        logging.exception(f"Exception while uploading input file {input} to S3 bucket {bucket} for AWS Comprehend job.")
-        raise
-
-# Download AWS Comprehend output from the given URL in the given S3 bucket to the tmpdir and extract it to aws_entities.
-def download_output_from_s3(outputuri, s3uri, bucket, tmpdir, aws_entities):
-    # get the output file from s3
-    try:
-        outkey = outputuri.replace(s3uri, '')
-        outname = outkey.rsplit("/", 1)[1]
-        output = tmpdir + outname
-        s3_client = boto3.client('s3', **amp.utils.get_aws_credentials())    
-        s3_client.download_file(bucket, outkey, output)
-        logging.info(f"Successfully downloaded AWS Comprehend output {outputuri} to compressed output file {output}.")
-    except Exception as e:
-        logging.exception(f"Exception while downloading AWS Comprehend output {outputuri} to compressed output file {output}.")
-        raise
-    
-    # extract the contents of the output.tar.gz file and move the uncompressed file to galaxy output
-    try:
-        tar = tarfile.open(output)
-        outputs = tar.getmembers()
-        tar.extractall()
-        tar.close()
-                
-        if len(outputs) > 0:
-            source = outputs[0].name
-            shutil.move(source, aws_entities) 
-            logging.info(f"Successfully uncompressed {output} to {source} and moved it to {aws_entities}.")
+    if args.lwlw:
+        # if the job exists, check it.  Otherwise, submit a new job.
+        if get_job(job_name):
+            rc = check_job(job_name, s3_bucket, job_name, args.aws_transcript)
         else:
-            raise Exception(f"Error: Compressed output file {output} does not contain any member.")
-    except Exception as e:
-        logging.exception(f"Exception while uncompressing/moving {output} to {aws_entities}.")
-        raise     
+            rc = submit_job(job_name, s3_bucket, amp_transcript_obj)
+        exit(rc)
+    else:
+        # synchronous operation (for testing)
+        rc = submit_job(job_name, s3_bucket, amp_transcript_obj)
+        while rc == 255:
+            rc = check_job(job_name, s3_bucket, job_name, args.aws_transcript)
+            time.sleep(10)
+        exit(rc)
 
-# Run AWS Comprehend job with the given jobname, using the input at the given S3 URL with the given dataAccessRoleArn.
-def run_comprehend_job(jobname, s3uri, dataAccessRoleArn):
-    # submit AWS Comprehend job
+
+
+def get_job(job_name):
+    "Return the comprehension job data or None if it doesn't exist"
+    comprehend = boto3.client(service_name='comprehend', **amp.utils.get_aws_credentials())    
+    response = comprehend.list_document_classification_jobs(Filter={'JobName': job_name})
+    if len(response.get('DocumentClassificationJobPropertiesList', [])):
+        return response['DocumentClassificationJobPropertiesList'][0]
+    else:
+        return None
+    
+  
+def submit_job(job_name, s3_bucket, amp_transcript):
+    "Submit the comprehend job"
+    # write the transcript data to a file and then upload it to S3
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            ifile = f"{tmpdir}/{job_name}"
+            with open(ifile, "w") as f:
+                f.write(amp_transcript.results.transcript)
+                logging.info(f"Successfully created transcript input file")
+
+            s3_client = boto3.client('s3', **amp.utils.get_aws_credentials())
+            response = s3_client.upload_file(ifile, s3_bucket, job_name)
+            logging.info(f"Successfully uploaded input file {ifile} to S3 bucket {s3_bucket} for AWS Comprehend job.")
+    except Exception as e:
+        logging.error("Failed to upload transcript: {e}")
+        return 1
+
+
     try:
         # TODO region name should be in MGM config
         comprehend = boto3.client(service_name='comprehend', **amp.utils.get_aws_credentials())
         # jobname was used as the object_name uploaded to s3
-        inputs3uri = s3uri + jobname
+        inputs3uri = f"s3://{s3_bucket}/{job_name}"
         response = comprehend.start_entities_detection_job(
             InputDataConfig={
                 'S3Uri': inputs3uri,
                 "InputFormat": "ONE_DOC_PER_FILE"
             },
             OutputDataConfig={
-                'S3Uri': s3uri
+                'S3Uri': f"s3://{s3_bucket}"
             },
             DataAccessRoleArn=dataAccessRoleArn,
-            JobName=jobname,
+            JobName=job_name,
             LanguageCode='en'
         )
         logging.info(f"Successfully submitted AWS Comprehend job with input {inputs3uri}.")
     except Exception as e:
         logging.exception(f"Exception while submitting AWS Comprehend job with input {inputs3uri}")
-        raise
+        cleanup_job(job_name)
+        return 1
 
+
+
+
+def check_job():
     # wait for AWS Comprehend job to end
     status = ''
     outputuri = ''
@@ -192,10 +151,69 @@ def run_comprehend_job(jobname, s3uri, dataAccessRoleArn):
     logging.debug(jobStatusResponse)     
     if status == 'COMPLETED':
         logging.info(f"AWS Comprehend job {jobname} completed in success with output {outputuri}.")  
-        return outputuri
+        
+        
+        # get the output file from s3
+        try:
+            outkey = outputuri.replace(s3uri, '')
+            outname = outkey.rsplit("/", 1)[1]
+            output = tmpdir + outname
+            s3_client = boto3.client('s3', **amp.utils.get_aws_credentials())    
+            s3_client.download_file(bucket, outkey, output)
+            logging.info(f"Successfully downloaded AWS Comprehend output {outputuri} to compressed output file {output}.")
+        except Exception as e:
+            logging.exception(f"Exception while downloading AWS Comprehend output {outputuri} to compressed output file {output}.")
+            raise
+        
+        # extract the contents of the output.tar.gz file and move the uncompressed file to galaxy output
+        try:
+            tar = tarfile.open(output)
+            outputs = tar.getmembers()
+            tar.extractall()
+            tar.close()
+                    
+            if len(outputs) > 0:
+                source = outputs[0].name
+                shutil.move(source, aws_entities) 
+                logging.info(f"Successfully uncompressed {output} to {source} and moved it to {aws_entities}.")
+            else:
+                raise Exception(f"Error: Compressed output file {output} does not contain any member.")
+        except Exception as e:
+            logging.exception(f"Exception while uncompressing/moving {output} to {aws_entities}.")
+            raise     
+
+                # Make call to AWS Comprehend
+        outputuri = run_comprehend_job(jobname, s3uri, role_arn)
+
+        # download AWS Comprehend output from s3 to the tmp directory, uncompress and copy it to output aws_entities output file
+        download_output_from_s3(outputuri, s3uri, s3_bucket, tmpdir, aws_entities)
+
+        # AWS Comprehend output should contain entities
+        aws_entities_json = amp.utils.read_json_file(aws_entities)
+        if not 'Entities' in aws_entities_json.keys():
+            logging.error(f"Error: AWS Comprehend output does not contain entities list")
+            exit(1)
+        aws_entities_list = aws_entities_json["Entities"]
+
+        # populate AMP Entities list based on input AMP transcript words list and output AWS Entities list  
+        amp.ner_helper.populate_amp_entities(amp_transcript_obj, aws_entities_list, amp_entities_obj, ignore_types_list)
+
+        # write the output AMP entities to JSON file
+        amp.utils.write_json_file(amp_entities_obj, amp_entities)
+        
+        logging.info("Finished.")
+        return outputuri  # should be return code
     else:
         raise Exception(f"Error: AWS Comprehend job {jobname} ended with status {status}.")
     
+
+
+
+
+def cleanup_job(job_name):
+    pass
+
+
 
 if __name__ == "__main__":
     main()
