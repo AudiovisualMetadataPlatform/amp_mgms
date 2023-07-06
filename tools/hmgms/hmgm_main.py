49f1f138c5970cfab434939406ea6a2e954b880b
Brian Wheeler <bdwheele@iu.edu>
2023-07-06 10:32:51 -0400

    first lwlw conversion

--- tools/hmgms/hmgm_main.py
+++ tools/hmgms/hmgm_main.py
@@ -8,13 +8,15 @@ import os.path
 import shutil
 from pathlib import Path
 import amp.logging
-from amp.config import load_amp_config, get_config_value, get_work_dir
+from amp.config import load_amp_config, get_work_dir
 from amp.fileutils import valid_file, create_empty_file, read_json_file
 from task.jira import TaskJira
 from task.manager import TaskManager
 from task.openproject import TaskOpenproject 
 from task.redmine import TaskRedmine
 from task.trello import TaskTrello
+from amp.lwlw import LWLW
+
 
 
 # It's assumed that all HMGMs generate the output file in the same directory as the input file with ".completed" suffix added to the original filename
@@ -30,59 +32,88 @@ def main():
 	# parse command line arguments
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--debug", default=False, action="store_true", help="Turn on debugging")
+	parser.add_argument("--lwlw", default=False, action="store_true", help="Use LWLW protocol")
 	parser.add_argument("task_type", help="type of HMGM task: (Transcript, NER, Segmentation, OCR), there is one HMGM wrapper per type")
 	parser.add_argument("input_json", help="input file for HMGM task in json format")
 	parser.add_argument("output_json", help="output file for HMGM task in json format")
 	parser.add_argument("task_json", help="json file storing information about the HMGM task, such as ticket # etc")
 	parser.add_argument("context_json", help="context info as json string needed for creating HMGM tasks")
+	
 	args = parser.parse_args()
 	amp.logging.setup_logging("hmgm_main", args.debug)
 	logging.debug(f"Starting with args {args}")
 
+	# basic sanity checking
+	logging.debug(f"Handling HMGM task: uncorrected JSON: {args.input_json}, corrected JSON: {args.output_json}, task JSON: {args.task_json}")
+	# clean up previous error file as needed in case this is a rerun of a failed job
+	Path(args.output_json + ".err").unlink(missing_ok=True)
+	# as a safeguard, if input_json doesn't exist or is empty, throw exception to fail the job
+	# (this means the conversion command failed before hmgm task command)
+	if not valid_file(args.input_json):
+		logging.error(f"{args.input_json} is not a valid file")
+		exit(LWLW.ERROR)
+	# if input_json has empty data (not empty file), no need to go through HMGM task, just copy it to the output file, and done
+	if empty_input(args.input_json, args.task_type):
+		logging.info(f"Input file {args.input_json} for HMGM {args.task_type} editor contains empty data, skipping HMGM task and copy the input to the output")
+		shutil.copy(args.input_json, args.output_json)
+		exit(LWLW.OK)
+	
+	# if we get this far, then it's OK to be an LWLW process.
 	# using output instead of input filename as the latter is unique while the former could be used by multiple jobs 
 	try:
-		# clean up previous error file as needed in case this is a rerun of a failed job
-		Path(args.output_json + ".err").unlink(missing_ok=True)
-		
-		# as a safeguard, if input_json doesn't exist or is empty, throw exception to fail the job
-		# (this means the conversion command failed before hmgm task command)
-		if not valid_file(args.input_json):
-			logging.error(f"{args.input_json} is not a valid file")
-			exit(1)
-		
-		logging.debug(f"Handling HMGM task: uncorrected JSON: {args.input_json}, corrected JSON: {args.output_json}, task JSON: {args.task_json}")				
-        # Load basic HMGM configuration based from the property file under the given root directory
-		config = load_amp_config()
-		context = json.loads(args.context_json)
-		context = desanitize_context(context)
-		
-		# if input_json has empty data (not empty file), no need to go through HMGM task, just copy it to the output file, and done
-		if empty_input(args.input_json, args.task_type):
-			logging.info(f"Input file {args.input_json} for HMGM {args.task_type} editor contains empty data, skipping HMGM task and copy the input to the output")
-			shutil.copy(args.input_json, args.output_json)
-            # implicitly exit 0 as the current command completes
-		# otherwise, if HMGM task hasn't been created, create one, exit 1 to get requeued	
-		elif not task_created(args.task_json):
-			task = create_task(config, args.task_type, context, args.input_json, args.output_json, args.task_json)
-			logging.info(f"Successfully created HMGM task {task.key}... uncorrected: {args.input_json}, corrected: {args.output_json}, task: {args.task_json}")
-			exit(255) 
-		# otherwise, check if HMGM task is completed
-		else:
-			editor_output = task_completed(config, args.output_json)
-			# if HMGM task is completed, close the task and move editor output to output file, and done
-			if (editor_output):
-				task = close_task(config, context, editor_output, args.output_json, args.task_json)
-				logging.info(f"Successfully closed HMGM task {task.key}. uncorrected: {args.input_json}, corrected: {args.output_json}, task: {args.task_json}")
-				# implicitly exit 0 as the current command completes
-			# otherwise exit 255 to get requeued
-			else:
-				logging.info(f"Waiting for HMGM task to complete ... uncorrected: {args.input_json}, corrected: {args.output_json}, task: {args.task_json}")
-				exit(255)        
+		hmgm = HMGM(args.task_type, args.input_json, args.output_json, args.task_json, args.context_json)
+		exit(hmgm.run(lwlw=args.lwlw))
 	# upon exception, create error file to notify the following conversion command to fail, and exit 1 (error) to avoid requene
 	except Exception as e:
 		create_empty_file(args.output_json + ".err")
 		logging.exception(f"Failed to handle HMGM task: uncorrected: {args.input_json}, corrected: {args.output_json}, task: {args.task_json}")
-		exit(1)
+		exit(LWLW.ERROR)
+
+	
+
+class HMGM(LWLW):
+	def __init__(self, task_type, input_json, output_json, task_json, context_json):
+		self.task_type = task_type
+		self.input_json = input_json
+		self.output_json = output_json
+		self.task_json = task_json		
+        # Load basic HMGM configuration 
+		self.config = load_amp_config()
+		# parse the context
+		self.context = desanitize_context(json.loads(context_json))
+		
+
+	def exists(self):
+		"Return the ID of the job or None if it doesn't exist"
+		# Since Galaxy creates all output files with size 0 upon starting a job, we can't use the existence of task_json file 
+		# to decide if the task has been created; rather, we can use its file size as the criteria
+		# In addition, we could call TaskManager to check the task in the actual task platform, but that's an extra overhead and there is a chance of the task site being down. 
+		# Since task_json is only created with task info after a task gets created successfully, we can use it as an indicator of task existence.
+		return self.task_json if valid_file(self.task_json) else None
+
+
+	def submit(self):
+		task = create_task(self.config, self.task_type, self.context, self.input_json, self.output_json, self.task_json)
+		logging.info(f"Successfully created HMGM task {task.key}... uncorrected: {self.input_json}, corrected: {self.output_json}, task: {self.task_json}")
+		return LWLW.WAIT
+
+	def check(self):		
+		editor_output = task_completed(self.config, self.output_json)
+		# if HMGM task is completed, close the task and move editor output to output file, and done
+		if (editor_output):
+			task = close_task(self.config, self.context, editor_output, self.output_json, self.task_json)
+			logging.info(f"Successfully closed HMGM task {task.key}. uncorrected: {self.input_json}, corrected: {self.output_json}, task: {self.task_json}")
+			return LWLW.OK
+		# otherwise exit 255 to get requeued
+		else:
+			logging.info(f"Waiting for HMGM task to complete ... uncorrected: {self.input_json}, corrected: {self.output_json}, task: {self.task_json}")
+			return LWLW.WAIT    
+
+
+	def cleanup(self):
+		"Clean up everything"
+		# we let galaxy do the cleanup since everything is local.
+		pass
 
 
 # Desanitize all the names in the given context.
@@ -117,13 +148,7 @@ def empty_input(input_json, task_type):
 	elif task_type == TaskManager.OCR:
 		return True
  
-# Return true if HMGM task has already been created, i.e. the file containing the HMGM task info exists.
-def task_created(task_json):    
-	# Since Galaxy creates all output files with size 0 upon starting a job, we can't use the existence of task_json file 
-	# to decide if the task has been created; rather, we can use its file size as the criteria
-	# In addition, we could call TaskManager to check the task in the actual task platform, but that's an extra overhead and there is a chance of the task site being down. 
-	# Since task_json is only created with task info after a task gets created successfully, we can use it as an indicator of task existence.
-	return valid_file(task_json)
+
 
 
 # If HMGM task has already been completed, i.e. the completed version of the given output JSON file exists, return the output file path; otherwise return False. 
